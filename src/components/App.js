71f9019e1cf79845f2564c2d17ecc9aa3d6f5b7e
Jason Baik <baikjs87@gmail.com>
2022-05-25 00:11:29 -0400

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React from "react"
+import About from "./About"
+import Home from "./Home"
+import NavBar from "./NavBar"
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<About />
+		</div>
+	)
 }
 
-export default App;
+export default App
